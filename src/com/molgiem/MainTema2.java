45d2a42268b2e0a761b687cebe913b01150a4ae8
Molgiem <maicolggarzonmelo@gmail.com>
2023-04-02 15:52:33 -0500

    Ejercicio Tema 2 Java Básico Correccion

--- src/com/molgiem/MainTema2.java
+++ src/com/molgiem/MainTema2.java
@@ -1,12 +1,15 @@
 package com.molgiem;
 
+import java.util.Scanner;
+
 public class MainTema2 {
     public static void main(String[] args) {
-        double precioInicial = 10.15;
+        Scanner sc = new Scanner(System.in);
+        int precioInicial = sc.nextInt();
         System.out.println("El precio final es de " + calcularIva(precioInicial) + " euros");
     }
 
-    public static double calcularIva(double precio){
+    public static double calcularIva(int precio){
         return precio * 1.16;
     }
 }
