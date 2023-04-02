7713b721e95f28e310c9967572c4ff5b0d01e27e
Molgiem <maicolggarzonmelo@gmail.com>
2023-04-02 15:10:00 -0500

    Ejercicio Tema 1 Java Básico

---
+++ src/com/molgiem/MainTema1.java
@@ -0,0 +1,20 @@
+package com.molgiem;
+
+public class MainTema1 {
+
+    public static void main(String[] args) {
+        int numeroInt = 42;
+        long numeroLong = 123456789123L;
+        double numeroDouble = 42.42d;
+        boolean bool = true;
+        String datoString = "Hola mundo";
+
+        System.out.println("Ejemplos de datos primitivos:");
+        System.out.println("Integer: " + numeroInt);
+        System.out.println("Long: " + numeroLong);
+        System.out.println("Double: " + numeroDouble);
+        System.out.println("Booleano: " + bool);
+        System.out.println("String: " + datoString);
+    }
+
+}
